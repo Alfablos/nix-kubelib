9c290704390fe3fed7ac98bc941ee3cff532c1b4
Antonio Galanti <antonio.galanti@gmail.com>
2025-01-14 15:07:14 +0100

    using flake-utils

--- flake.nix
+++ flake.nix
@@ -1,20 +1,29 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      # inherit (pkgs) lib;
-      kubelib = pkgs.callPackage ./lib { };
-    in
     {
-      packages.${system}.default = self.packages.lib;
-      packages.lib = kubelib;
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ yq-go ];
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+        # inherit (pkgs) lib;
+        kubelib = pkgs.callPackage ./lib { };
+      in
+      {
+        packages.default = self.packages.lib;
+        packages.lib = kubelib;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ yq-go ];
+        };
+      }
+
+    );
 }
